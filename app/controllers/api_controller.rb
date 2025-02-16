11dd992f58c2df2f2ef16a232bc7a8bfe1e048a5
Gabriel Gavrilov <gabriel.gavrilov02@gmail.com>
2025-02-15 21:10:31 -0600

    Map /api/check_email

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,12 +1,18 @@
 class ApiController < ApplicationController
+
+  skip_before_action :verify_authenticity_token
+
   def check_email
+    email = params[:email]
     validated_params = params.require(:email)
-    @user = User.find_by(validated_params)
+    @user = User.find_by(email: email)
 
     if @user.nil?
       render json: { button_txt: "Rent a VPS" }, status: :not_found
+      return
     end
 
     render json: { button_txt: "Go to dashboard" }, status: :ok
   end
+
 end
