027672558789b14eef5c789bc7baad9603e59089
Gabriel Gavrilov <gabriel.gavrilov02@gmail.com>
2025-02-17 18:42:03 -0600

    Initial commit

--- app/services/send_email_service.rb
+++ app/services/send_email_service.rb
@@ -1,4 +1,15 @@
 class SendEmailService
     def execute()
+        mg_client = Mailgun::Client.new(ENV["MAILGUN_API_KEY"])
+
+        messages_params = {
+            from: "no-reply@getserver.app",
+            to: "gabriel.gavrilov02@gmail.com",
+            subject: "Test email",
+            text: "This is a test email."
+        }
+
+        mg_client.send_message("getserver.app", messages_params)
+
     end
 end
